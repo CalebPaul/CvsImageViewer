c19b1abeab86fbc1f48aeb92c75416f5bd866cf2
Caleb Paul <calebpaul21@gmail.com>
2022-08-18 13:55:28 -0700

    Create Constant.kt

---
+++ app/src/main/java/dev/calebcodes/cvsimageviewer/util/Constant.kt
@@ -0,0 +1,7 @@
+package dev.calebcodes.cvsimageviewer.util
+
+object Constant {
+    const val BASE_URL = "https://api.flickr.com/services/feeds/"
+    const val FORMAT = "json"
+    const val NO_JSON_CALLBACK = "1"
+}
