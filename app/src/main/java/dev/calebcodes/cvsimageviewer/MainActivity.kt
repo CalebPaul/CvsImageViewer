aaccbca47f084c1f215a342a1a2ffefe2d071333
Caleb Paul <calebpaul21@gmail.com>
2022-08-18 14:04:17 -0700

    Update MainActivity.kt

--- app/src/main/java/dev/calebcodes/cvsimageviewer/MainActivity.kt
+++ app/src/main/java/dev/calebcodes/cvsimageviewer/MainActivity.kt
@@ -3,41 +3,50 @@ package dev.calebcodes.cvsimageviewer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.lifecycle.viewmodel.compose.LocalViewModelStoreOwner
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import dagger.hilt.android.AndroidEntryPoint
+import dev.calebcodes.cvsimageviewer.navigation.Screen
+import dev.calebcodes.cvsimageviewer.ui.ImageListScreen
+import dev.calebcodes.cvsimageviewer.ui.detailScreen.ImageDetailScreen
 import dev.calebcodes.cvsimageviewer.ui.theme.CvsImageViewerTheme
+import dev.calebcodes.cvsimageviewer.viewmodel.ImageListViewModel
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             CvsImageViewerTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                val viewModelStoreOwner = checkNotNull(LocalViewModelStoreOwner.current)
+                val navController = rememberNavController()
+                NavHost(
+                    navController = navController,
+                    startDestination = Screen.ImageListScreen.route
                 ) {
-                    Greeting("Android")
+                    composable(
+                        route = Screen.ImageListScreen.route
+                    ) {
+                        val model = viewModel<ImageListViewModel>(viewModelStoreOwner = viewModelStoreOwner)
+                        ImageListScreen(model, navController)
+                    }
+                    composable(
+                        route = Screen.ImageDetailScreen.route + "/{imageId}",
+                        arguments = listOf(navArgument("imageId") { type = NavType.StringType })
+                    ) { backStackEntry ->
+                        CompositionLocalProvider(LocalViewModelStoreOwner provides viewModelStoreOwner) {
+                            val model = viewModel<ImageListViewModel>()
+                            ImageDetailScreen(model, backStackEntry.arguments?.getString("imageId"))
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    CvsImageViewerTheme {
-        Greeting("Android")
     }
 }
